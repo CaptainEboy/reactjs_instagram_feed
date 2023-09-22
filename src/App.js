92bf7bf47967c630fe4fb2d29cac3e32bafba78d
CaptainEboy <emaapps.ac24@gmail.com>
2023-09-22 07:41:24 +0100

    still tseting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <header className="App-header" style={{textAlign:'center'}}>
-        <h1>Instagram Feed with Instagram API</h1>
+        <h1>Instagram Feed with Instagram API.</h1>
       </header>
 
       <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12}/>
