cfc0a33cf7011cf4234992222320c7464aacc50f
CaptainEboy <emaapps.ac24@gmail.com>
2023-10-28 03:23:25 +0100

    tired

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 const App = () => {
   return (
     <>
-      <header className="App-header" style={{textAlign:'center', backgroundColor:'#262626', color:'white'}}>
-        <h1 style={{color:'white'}}>Welcome To Coal Ibogun</h1>
+      <header className="App-header" style={{textAlign:'center'}}>
+        <h1>Welcome To Coal Ibogun</h1>
       </header>
 
       <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={20}/>
