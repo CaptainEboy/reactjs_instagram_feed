af66950bd615341459382374af46d78f76091742
CaptainEboy <emaapps.ac24@gmail.com>
2023-10-28 03:19:26 +0100

    fixed theme title white

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <header className="App-header" style={{textAlign:'center', backgroundColor:'#262626', color:'white'}}>
-        <h1>Welcome To Coal Ibogun</h1>
+        <h1 style={{color:'white'}}>Welcome To Coal Ibogun</h1>
       </header>
 
       <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={20}/>
